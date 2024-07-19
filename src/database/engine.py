fea4165a729743675abfe189ab9ef2e970a52a03
Björn Schrammel <github@schrammel.dev>
2024-07-19 06:38:39 +0200

    Refactoring database logic

--- src/database/engine.py
+++ src/database/engine.py
@@ -1,109 +1,106 @@
 import os
 import sqlite3
 
-from multiprocessing import Lock
+from threading import Lock
+from typing import Union, List
 
 from src.database.query_builder import Select, Insert, Update, Delete, CreateTable, Query, CreateTrigger
 
 __all__ = ["Engine"]
 
 
-class Engine:
+class DatabaseConnection(sqlite3.Connection):
     def __init__(self):
-        self.connection, self.cursor = self.connect()
-        self.lock = Lock()
-
-    # Connection management
-    def connect(self):
-        conn = sqlite3.connect(os.environ.get("DB_HOST"))
-        return conn, conn.cursor()
-
-    def disconnect(self):
-        self.connection.close()
+        self.db_path = os.environ["DB_HOST"]
+        super().__init__(self.db_path)
+        self.row_factory = sqlite3.Row
 
     def __enter__(self):
-        if not self.connection:
-            self.connection, self.cursor = self.connect()
-        return self
+        return self.cursor()
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.disconnect()
-        return True
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.commit()
+        self.close()
+    
+
+class Engine:
+    def __init__(self):
+        self.dbc = DatabaseConnection()
+        self.lock = Lock()
 
     # Query execution
     def _execute(
         self,
         query: str,
         parameters: dict = None,
-        fetch: bool = False,
-        fetchall: bool = False,
-        lastrowid: bool = False,
-        rowcount: bool = False,
-        commit: bool = True
+        commit: bool = True,
+        fetch_method: str = None
     ):
         with self.lock:
             try:
                 if parameters:
-                    self.cursor.execute(query, parameters)
+                    self.dbc.execute(query, parameters)
                 else:
-                    self.cursor.execute(query)
+                    self.dbc.execute(query)
             except sqlite3.Error as e:
                 print(query)
                 print(parameters)
                 raise e
 
-            if query.startswith("CREATE TABLE") and "update_at" in query:
-                self.cursor.execute(CreateTrigger(
-                    table_name=query.split(" ")[2],
-                    trigger_name="update_at",
-                    action="UPDATE",
-                    timing="AFTER",
-                    body=Update(
-                        table_name=query.split(" ")[2],
-                        columns={"updated_at": "CURRENT_TIMESTAMP"},
-                        where={"id": "OLD.id"}
-                    )).query
-                )
-
             if commit:
-                self.connection.commit()
-            if fetch:
-                return self.cursor.fetchone()
-            if fetchall:
-                return self.cursor.fetchall()
-            if lastrowid:
-                return self.cursor.lastrowid
-            if rowcount:
-                return self.cursor.rowcount
+                self.dbc.commit()
+
+            fm = getattr(self.dbc.cursor, fetch_method, None)
+            return fm() if fm else None
 
     def execute(self, query: "Query") -> "sqlite3.Cursor":
         return self._execute(query.query, query.parameters)
 
-    # CRUD operations
-    def select(self, **kwargs) -> list:
+    def crud(self, fetch_method, **kwargs) -> Union["sqlite3.Row", "sqlite3.Cursor", List["sqlite3.Row"], int]:
         query = Select(**kwargs)
-        return self._execute(query.query, query.parameters, fetchall=True)
+        return self._execute(query.query, query.parameters, fetch_method=fetch_method)
+
+    # CRUD operations
+    def select(self, **kwargs) -> List["sqlite3.Row"]:
+        return self.crud("fetchall", **kwargs)
+
+    def select_one(self, **kwargs) -> "sqlite3.Row":
+        return self.crud("fetchone", **kwargs)
 
     def insert(self, **kwargs) -> int:
-        query = Insert(**kwargs)
-        return self._execute(query.query, query.parameters, lastrowid=True)
+        return self.crud("lastrowid", **kwargs)
 
     def update(self, **kwargs) -> int:
-        query = Update(**kwargs)
-        return self._execute(query.query, query.parameters, rowcount=True)
+        return self.crud("rowcount", **kwargs)
 
     def delete(self, **kwargs) -> int:
-        query = Delete(**kwargs)
-        return self._execute(query.query, query.parameters, rowcount=True)
+        return self.crud("rowcount", **kwargs)
 
     # Table management
     def create_table(self, table_name, **kwargs) -> None:
         query = CreateTable(table_name=table_name, **kwargs)
         return self._execute(query.query, query.parameters)
 
-    def create_view(self, view_name, **kwargs) -> None:
-        self._execute(f"CREATE VIEW {'IF NOT EXISTS ' if kwargs.pop('can_exist') else ''}{view_name} AS {Select(**kwargs).with_parameters()}")
+    def create_view(self, view_name: str, **kwargs) -> None:
+        if not view_name:
+            raise ValueError("View name must be provided and cannot be empty.")
+
+        # Assuming 'can_exist' is an optional boolean parameter in kwargs
+        can_exist = kwargs.pop('can_exist', False)
+        view_existence_clause = "IF NOT EXISTS " if can_exist else ""
+
+        # Construct the SELECT statement part of the view creation
+        select_statement = Select(**kwargs).with_parameters()
+
+        # Construct the full CREATE VIEW statement
+        create_view_statement = f"CREATE VIEW {view_existence_clause}{view_name} AS {select_statement}"
+
+        try:
+            self._execute(create_view_statement)
+        except sqlite3.Error as e:
+            # Handle specific database errors if needed
+            raise RuntimeError(f"Failed to create view '{view_name}': {e}")
 
     def show_tables(self) -> list:
         query = "SELECT name FROM sqlite_master WHERE type='table';"
-        return self._execute(query, fetchall=True)
+        return self._execute(query, fetch_method="fetchall")
--- src/timewise.py
+++ src/timewise.py
@@ -1,6 +1,7 @@
 from .database.engine import Engine as Database
 from .database.query_builder import Column, ForeignKey, UniqueConstraint, CheckConstraint
 from .secrets_manager import SecretsManager
+from threading import Lock
 
 
 class TimeWise:
@@ -13,10 +14,12 @@ class TimeWise:
     """
 
     _instance = None
+    _lock = Lock()
 
     def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self, database: Database, secrets_manager: SecretsManager):

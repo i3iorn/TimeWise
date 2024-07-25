17ef46c95ee377ec9470f29ce1ffd6e26f5dd218
bjosch <github@bjosch.tk>
2024-07-25 11:59:08 +0200

    Adding some profiling while working on the basics

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,18 @@
+# Import cprofiler
+import cProfile
+import pstats
+
 if __name__ == '__main__':
+    profiler = cProfile.Profile()
+
+    profiler.enable()
 
     from src.timewise import TimeWise
 
     app = TimeWise()
+    task = app.add_task("MyFirstTask")
+    print(task)
+
+    profiler.disable()
+    stats = pstats.Stats(profiler)
+    stats.sort_stats('tottime').print_stats(10)
